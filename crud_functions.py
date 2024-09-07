67ede3b135150b8e8752dd14dd4033537e37b04e
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-09-07 23:06:38 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,17 +1,27 @@
 import sqlite3
 
-connection = sqlite3.connect('product_base.db')
+connection = sqlite3.connect('product_base_v2.db')
 cursor = connection.cursor()
 
 
 def initiate_db():
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Products(
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    description TEXT,
-    price INT NOT NULL
-    );
+        CREATE TABLE IF NOT EXISTS Products(
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        price INT NOT NULL
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INT NOT NULL,
+        balance INT NOT NULL
+        );
     ''')
 
 
@@ -21,9 +31,25 @@ def get_all_products():
     return products
 
 
+def add_user(username, email, age):
+    cursor.execute(f'INSERT INTO Users (username, email, age, balance) VALUES("{username}", "{email}", {age}, 1000);')
+    connection.commit()
+
+
+def is_included(username):
+    user = cursor.execute(f'SELECT * FROM Users WHERE username = "{username}";').fetchone()
+    connection.commit()
+    if user is None:
+        return False
+    else:
+        return True
+
+
 # initiate_db()
 # print(get_all_products())
-
+# add_user('Murad', 'example@mail.ru', 48)
+# if is_included('Noname'):
+#     print('Ok')
 
 connection.commit()
 # connection.close()
